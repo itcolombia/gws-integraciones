795c2338c3a2bcfbe64455bf6261d578ab0e0f19
github-ek <esb.ega.kat@gmail.com>
2018-07-10 15:36:43 -0500

    Revert "Merge branch 'master' of https://github.com/itcolombia/gws-integraciones"
    This reverts commit 9007ab29080b63f6ce8fd580c6b15c58ed59d16d, reversing
    changes made to 6bf85283539ed1132fb9d23f6541a75a5f5dacd0.

--- gws-core/src/main/java/com/gws/integraciones/enums/EstadoRegistroType.java
+++ gws-core/src/main/java/com/gws/integraciones/enums/EstadoRegistroType.java
@@ -1,13 +1,23 @@
 package com.gws.integraciones.enums;
 
 public enum EstadoRegistroType {
-	// @formatter:off
+	 // @formatter:off
 
-	    MIGRADO,
-	    RECIBIDO,
-	    CORREGIDO,
-	    PROCESADO,
-	    DESCARTADO,
-	    ERROR,
+    NO_PROCESADO,
+    ESTRUCTURA_VALIDA,
+    CORREGIDO,
+
+    ERROR_ENRIQUECIMIENTO,
+    ERROR_HOMOLOGACION,c
+    HOMOLOGADO,
+
+    ERROR_VALIDACION,
+    VALIDADO,
+    DESCARTADO,
+
+    ERROR_CARGUE,
+    DETENIDO,
+    PROCESADO,
+
        // @formatter:on
 }
