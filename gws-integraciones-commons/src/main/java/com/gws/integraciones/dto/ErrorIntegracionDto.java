4f31f2ad3e6f8d58d676ca0c95a6b52aed5049df
Diego Moya <itcolombia@uetainc.com>
2018-07-10 18:16:14 -0500

    Agregar errores

---
+++ gws-integraciones-commons/src/main/java/com/gws/integraciones/dto/ErrorIntegracionDto.java
@@ -0,0 +1,69 @@
+package com.gws.integraciones.dto;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorIntegracionDto {
+
+	private Integer id;
+	@NotNull
+	@Size(max = 50)
+	private String integracion;
+	@NotNull
+	@Size(max = 100)
+	private String idExterno;
+	@NotNull
+	@Size(max = 50)
+	private String codigo;
+	@NotNull
+	private String mensaje;
+	@NotNull
+	@Size(max = 100)
+	private String arg0;
+	@NotNull
+	@Size(max = 100)
+	private String arg1;
+	@NotNull
+	@Size(max = 100)
+	private String arg2;
+	@NotNull
+	@Size(max = 100)
+	private String arg3;
+	@NotNull
+	@Size(max = 100)
+	private String arg4;
+	@NotNull
+	@Size(max = 100)
+	private String arg5;
+	@NotNull
+	@Size(max = 100)
+	private String arg6;
+	@NotNull
+	@Size(max = 100)
+	private String arg7;
+	@NotNull
+	@Size(max = 100)
+	private String arg8;
+	@NotNull
+	@Size(max = 100)
+	private String arg9;
+	private int version;
+	@NotNull
+	private LocalDateTime fechaCreacion;
+	@NotNull
+	private LocalDateTime fechaModificacion;
+
+}
--- gws-integraciones-commons/src/main/java/com/gws/integraciones/enums/EstadoRegistroType.java
+++ gws-integraciones-commons/src/main/java/com/gws/integraciones/enums/EstadoRegistroType.java
@@ -1,5 +1,12 @@
 package com.gws.integraciones.enums;
 
 public enum EstadoRegistroType {
-
+	// @formatter:off
+	    MIGRADO,
+	    RECIBIDO,
+	    CORREGIDO,
+	    PROCESADO,
+	    DESCARTADO,
+	    ERROR,
+	// @formatter:on
 }
